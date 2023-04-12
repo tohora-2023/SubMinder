7fdb03c6bb02bc7ef0a374ed4f2a563156ce421f
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-13 00:03:42 +1200

    setup login page

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -3,6 +3,28 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { redirect } from 'react-router-dom'
 
 export default function LogIn() {
+  const { user, logout, loginWithRedirect } = useAuth0()
 
-  return(<div> log in</div>)
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+  function handleKeyDownLogin(e: React.KeyboardEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    if (e.code == 'Enter') {
+      loginWithRedirect()
+    }
+  }
+  return (
+    <div>
+      {' '}
+      <button
+        type="button"
+        onClick={handleSignIn}
+        onKeyDown={handleKeyDownLogin}
+        tabIndex={0}
+      >
+        log in
+      </button>
+    </div>
+  )
 }
