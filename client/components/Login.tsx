a734086df9ba1edc134d624c2620748291f513ab
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-13 01:12:30 +1200

    setup logIn componant

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import { redirect } from 'react-router-dom'
+import addLogInInfo from '../apis/login'
+
+interface UserLogInProp {
+  firstName?: string
+  lastName?: string
+  userName?: string
+  image?: string
+}
 
 export default function LogIn() {
-  const { user, logout, loginWithRedirect } = useAuth0()
+  const { loginWithRedirect, getAccessTokenSilently } = useAuth0()
 
-  const handleSignIn = () => {
+  const handleSignIn = async () => {
+    const token = await getAccessTokenSilently()
+    const userInfo: UserLogInProp = {}
+    await addLogInInfo(userInfo, token)
     loginWithRedirect()
   }
   function handleKeyDownLogin(e: React.KeyboardEvent<HTMLButtonElement>) {
     e.preventDefault()
     if (e.code == 'Enter') {
-      loginWithRedirect()
+      handleSignIn()
     }
   }
   return (
