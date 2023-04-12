9a45c91932fd8950723c81e4bfc25ca66191bc55
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-12 16:12:10 +1200

    setting up auth

--- client/index.tsx
+++ client/index.tsx
@@ -1,12 +1,27 @@
 import { createRoot } from 'react-dom/client'
+
 import { Provider } from 'react-redux'
 import store from './store'
 
 import App from './components/App'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const root = createRoot(document.getElementById('app') as HTMLElement)
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  /**
+   * Auth0Provider is a component that has a hook that provides
+   * all authentication operations
+   *
+   * TODO: replace the empty strings below with your own domain, clientId, and audience
+   */
+  <Auth0Provider
+    domain=""
+    clientId=""
+    // redirectUri={window.location.origin}
+    // audience=""
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Auth0Provider>
 )
--- server/routes/fruits.ts
+++ server/routes/fruits.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+
 import * as db from '../db/fruits'
 
 const router = Router()
