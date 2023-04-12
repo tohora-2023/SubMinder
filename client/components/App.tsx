30eda633eb049b7371d4ce5d4448489f18a1e2a6
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-12 17:16:21 +1200

    setup App conditional

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,10 +2,13 @@ import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks'
 
 import { fetchFruits } from '../actions'
+import LogIn from './Login'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
   const fruits = useAppSelector((state) => state.fruits)
   const dispatch = useAppDispatch()
+  const { isAuthenticated } = useAuth0()
 
   useEffect(() => {
     dispatch(fetchFruits())
@@ -13,16 +16,20 @@ function App() {
 
   return (
     <>
-      <div className="mt-8 flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold">Fullstack Boilerplate</h1>
-        <ul className="mt-4 flex flex-col gap-4 rounded-lg bg-slate-300 px-8 py-4">
-          {fruits.map((fruit) => (
-            <li className="font-semibold text-slate-900" key={fruit}>
-              {fruit}
-            </li>
-          ))}
-        </ul>
-      </div>
+      {isAuthenticated ? (
+        <div className="mt-8 flex flex-col items-center justify-center">
+          <h1 className="text-3xl font-bold">Fullstack Boilerplate</h1>
+          <ul className="mt-4 flex flex-col gap-4 rounded-lg bg-slate-300 px-8 py-4">
+            {fruits.map((fruit) => (
+              <li className="font-semibold text-slate-900" key={fruit}>
+                {fruit}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <LogIn />
+      )}
     </>
   )
 }
---
+++ client/components/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { redirect } from 'react-router-dom'
+
+export default function LogIn() {
+
+  return(<div> log in</div>)
+}
