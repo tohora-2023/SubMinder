37cd20ee3d58bcdbd24f2e407c2eee4688194e9e
Shiraz Buksh <shiraz19@live.com>
2023-04-13 15:49:22 +1200

    nav bar styled

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,43 +1,49 @@
 function Nav() {
-  const user = {
-    username: 'ida',
-  }
-
   return (
     <>
-      <nav className="flex w-full flex-col bg-white py-4 px-6 text-center shadow sm:flex-row sm:items-baseline sm:justify-between sm:text-left">
-        <div className="mb-2 sm:mb-0">
+      <nav className=" flex flex w-full items-center justify-center bg-purple-900 py-2 px-4 sm:flex-row sm:justify-between sm:text-left">
+        <div className="mb-2 sm:mb-0 ">
           <a
             href="/"
-            className="text-grey-darkest hover:text-blue-dark text-2xl no-underline"
+            className="hover:text-blue-dark text-2xl text-white no-underline"
           >
-            Home
+            <img
+              className="h-20 w-20"
+              src="/images/subminder-logo.png"
+              alt="home"
+            ></img>
           </a>
         </div>
         <div>
           <a
             href="/paymenthistory"
-            className="text-grey-darkest hover:text-blue-dark ml-2 text-lg no-underline"
+            className="hover:text-blue-dark mr-10 ml-10 text-lg  text-white no-underline"
           >
             Payment History
           </a>
           <a
             href="/managesubscriptions"
-            className="text-grey-darkest hover:text-blue-dark ml-2 text-lg no-underline"
+            className="hover:text-blue-dark mr-10 ml-10 text-lg  text-white no-underline"
           >
             Manage Subscriptions
           </a>
           <a
             href="/addsubscription"
-            className="text-grey-darkest hover:text-blue-dark ml-2 text-lg no-underline"
+            className="hover:text-blue-dark mr-10 ml-10 text-lg  text-white no-underline"
           >
             Add Subscription
           </a>
           <a
             href="/profile"
-            className="text-grey-darkest hover:text-blue-dark ml-2 text-lg no-underline"
+            className="hover:text-blue-dark mr-10 ml-10 text-lg text-white no-underline"
           >
             Profile
+          </a>
+          <a
+            href="/profile"
+            className="hover:text-blue-dark mr-10 ml-10 text-lg text-white no-underline"
+          >
+            Sign Out
           </a>
         </div>
       </nav>
--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
+import './styles/index.css'
 
 import App from './components/App'
 
