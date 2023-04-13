c0e8b2ec5823365376c0b5ce7384238027d8df5b
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-13 14:22:41 +1200

    final changes

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -44,6 +44,7 @@ function App() {
   return (
     <>
       {isAuthenticated ? (
+        //please add stuff here
         <div className="mt-8 flex flex-col items-center justify-center">
           <h1 className="text-3xl font-bold">Fullstack Boilerplate</h1>
           <button type="button" onClick={handleSignOut}>
