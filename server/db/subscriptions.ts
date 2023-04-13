a1d06bf919b27321ba58355c38b4b4817a15b5cf
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-04-13 12:40:10 +1200

    Rewrite database function for getting subscriptions

--- server/db/subscriptions.ts
+++ server/db/subscriptions.ts
@@ -3,3 +3,17 @@ import connection from './connection'
 export function getAllSubs(db = connection) {
   return db('subscriptions').select()
 }
+
+export function getSubsWithDate(db = connection) {
+  return db('subscriptions')
+    .join('calendarEvents', 'subscriptions.id', 'calendarEvents.subscriptionId')
+    .select(
+      'subscriptions.name as name',
+      'subscriptions.category as category',
+      'subscriptions.endDate as endDate',
+      'calendarEvent.isLastPayment as isLastPayment',
+      'calendarEvent.scheduleDate as scheduleDate',
+      'subscriptions.price as price',
+      'subscriptions.website as website'
+    )
+}
--- server/routes/subscriptions.ts
+++ server/routes/subscriptions.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllSubs } from '../db/subscriptions'
+import { getAllSubs, getSubsWithDate } from '../db/subscriptions'
 import server from '../server'
 import { Subscription } from '../../models/subscription'
 
@@ -10,6 +10,6 @@ export default router
 //localhost:3000/v1/subscriptions
 
 router.get('/', async (req, res) => {
-  const subscriptions = await getAllSubs()
+  const subscriptions = await getSubsWithDate()
   res.json(subscriptions)
 })
