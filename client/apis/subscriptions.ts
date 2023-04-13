b1233ff440ae9c4ea6b9a537ab815461518dc760
keri van der Westhuizen <keri.vdw@gmail.com>
2023-04-14 06:01:55 +1200

    update the subscription formatting to check for undefined

--- client/apis/subscriptions.ts
+++ client/apis/subscriptions.ts
@@ -6,19 +6,26 @@ export function getSubscriptions(token: string): Promise<Subscription[]> {
     .get('/v1/subscriptions')
     .set('Authorization', `Bearer ${token}`)
     .then((res) => {
-      // const response = res.body
-      // const subscriptions = response.map((subscription: Subscription) => {
-      //Format fields for display
-      // subscription.name = subscription.name.toUpperCase()
-      // subscription.category = subscription.category.toLowerCase()
-      // subscription.frequency = subscription.frequency.toLowerCase()
-      //Format the date to 1 May
-      // const date = new Date(subscription.scheduleDate)
-      // subscription.scheduleDate = date.toLocaleDateString('en-GB', {
-      //   day: 'numeric',
-      //   month: 'long',
-      // })
+      const response = res.body
+      if (!response) return []
 
-      return res.body
+      const subscriptions = response.map((subscription: Subscription) => {
+        // Format fields for display
+        subscription.name = subscription.name?.toUpperCase() ?? ''
+        subscription.category = subscription.category?.toLowerCase() ?? ''
+        subscription.frequency = subscription.frequency?.toLowerCase() ?? ''
+
+        // Format the date to 1 May
+        const date = new Date(subscription.scheduleDate)
+        subscription.scheduleDate = date.toLocaleDateString('en-GB', {
+          day: 'numeric',
+          month: 'long',
+        })
+
+        return subscription
+      })
+
+      return subscriptions
     })
+    .catch(() => 'There was an error getting subscriptions')
 }
