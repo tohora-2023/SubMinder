619c69a0a82d0608abdb351bac220521253fcbec
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-15 02:49:16 +1200

    feat: add more options to frequancy

--- client/helper/CallenderEvents.ts
+++ client/helper/CallenderEvents.ts
@@ -21,7 +21,9 @@ export default function manageCalendarEvents(
     })
 
     let interval = 0
-    if (frequency === 'weekly') {
+    if (frequency === 'daily') {
+      interval = 1
+    } else if (frequency === 'weekly') {
       interval = 7
     } else if (frequency === 'fortnightly') {
       interval = 14
@@ -31,6 +33,12 @@ export default function manageCalendarEvents(
         currentDate.getMonth() + 1,
         0
       ).getDate()
+    } else if (frequency === 'quarterly') {
+      interval = 3 * 30
+    } else if (frequency === 'semiannually') {
+      interval = 6 * 30
+    } else if (frequency === 'yearly') {
+      interval = 365
     }
     currentDate = new Date(currentDate.getTime() + interval * 86400000) // 86400000 = 1 day in milliseconds
 
