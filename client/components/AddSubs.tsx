264c9e6ec686fc266e2ccc074774c5fcfa475d08
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-15 02:42:33 +1200

    feat: setup style and catagory in addSubs

--- client/components/AddSubs.tsx
+++ client/components/AddSubs.tsx
@@ -90,6 +90,7 @@ export default function AddSubs() {
         <label htmlFor="frequency">Frequency </label>
 
         <select
+          className="block w-full rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
           value={frequency}
           onChange={(e) => setFrequency(e.target.value)}
         >
@@ -115,6 +116,7 @@ export default function AddSubs() {
         />
 
         <br />
+
         <label htmlFor="endDate">End Date </label>
         <input
           type="date"
@@ -126,14 +128,19 @@ export default function AddSubs() {
         />
         <br />
         <label htmlFor="category">Category: </label>
-        <input
-          type="text"
-          id="category"
-          name="category"
+        <select
+          className="block w-full rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
           value={category}
           onChange={(e) => setCategory(e.target.value)}
-          className="focus:ring-primary block w-full border-gray-400 px-4 py-2 leading-5 placeholder-gray-500 focus:border-transparent focus:outline-none focus:ring-2"
-        />
+        >
+          <option value="">Select Category</option>
+          <option value="Food & Drink">Food & Drink</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Necessities">Necessities</option>
+          <option value="Bills">Bills</option>
+          <option value="Productivity">Productivity</option>
+          <option value="Travel">Travel</option>
+        </select>
 
         <br />
         <label htmlFor="website">Website: </label>
