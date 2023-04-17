1e70161c8944c9abae0a002e729632b7f6703565
Jamie Truman <121926321+jamie-truman@users.noreply.github.com>
2023-04-17 16:41:14 +1200

    changed profile component

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -21,15 +21,9 @@ export default function Profile() {
   return (
     <>
       <div className="flex-col justify-center text-center">
-        <button className="mb-5 mr-2 mt-5 rounded bg-blue-500 px-4 py-2 hover:bg-red-700">
-          View All Posts by User
-        </button>
         <p className="mb-5">{user.userName}</p>
         <p className="mb-5">{user.firstName}</p>
         <p className="mb-5">{user.lastName}</p>
-        <button className="mb-5 self-center rounded bg-blue-500 px-4 py-2 hover:bg-red-700">
-          Edit Profile
-        </button>
       </div>
     </>
   )
