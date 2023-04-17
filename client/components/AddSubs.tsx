e5e27898459df3db53033b93bac96ec504d6209c
Keri van der Westhuizen <75972814+kerivdw@users.noreply.github.com>
2023-04-17 17:43:29 +1200

    add validation to the add subscription form

--- client/components/AddSubs.tsx
+++ client/components/AddSubs.tsx
@@ -36,6 +36,10 @@ export default function AddSubs() {
   async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
 
+    if (price === 0) {
+      return alert('Price must be greater than 0')
+    }
+
     const newSub = {
       name,
       frequency,
@@ -94,6 +98,7 @@ export default function AddSubs() {
           value={name}
           onChange={(e) => setName(e.target.value)}
           className="focus:ring-primary block w-full border-gray-400 px-4 py-2 leading-5 placeholder-gray-500 focus:border-transparent focus:outline-none focus:ring-2"
+          required
         />
         <br />
         <label htmlFor="frequency">Frequency </label>
@@ -102,6 +107,7 @@ export default function AddSubs() {
           className="block w-full rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
           value={frequency}
           onChange={(e) => setFrequency(e.target.value)}
+          required
         >
           <option value="">Select Frequency</option>
           <option value="daily">Daily</option>
@@ -122,6 +128,7 @@ export default function AddSubs() {
           value={startDate.toISOString().slice(0, 10)}
           onChange={(e) => setStartDate(new Date(e.target.value))}
           className="focus:ring-primary block w-full border-gray-400 px-4 py-2 leading-5 placeholder-gray-500 focus:border-transparent focus:outline-none focus:ring-2"
+          required
         />
 
         <br />
@@ -133,6 +140,7 @@ export default function AddSubs() {
           value={endDate.toISOString().slice(0, 10)}
           onChange={(e) => setEndDate(new Date(e.target.value))}
           className="focus:ring-primary block w-full border-gray-400 px-4 py-2 leading-5 placeholder-gray-500 focus:border-transparent focus:outline-none focus:ring-2"
+          required
         />
         <br />
         <label htmlFor="category">Category: </label>
@@ -140,6 +148,7 @@ export default function AddSubs() {
           className="block w-full rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
           value={category}
           onChange={(e) => setCategory(e.target.value)}
+          required
         >
           <option value="">Select Category</option>
           <option value="Food & Drink">Food & Drink</option>
@@ -169,6 +178,7 @@ export default function AddSubs() {
           value={price}
           onChange={(e) => setPrice(Number(e.target.value))}
           className="focus:ring-primary block w-full border-gray-400 px-4 py-2 leading-5 placeholder-gray-500 focus:border-transparent focus:outline-none focus:ring-2"
+          required
         />
         <br />
 
