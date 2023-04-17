ded343223c32ae2433186d303a92c89510867a8e
Roisin Yates <roisinyates@outlook.com>
2023-04-18 09:52:22 +1200

    update events function to filter by AuthId

--- server/db/events.ts
+++ server/db/events.ts
@@ -1,15 +1,20 @@
 import { Subscription } from 'react-redux'
 import connection from './connection'
 
-export function getEvents(db = connection): Promise<Subscription[]> {
+export function getEvents(
+  id: string,
+  db = connection
+): Promise<Subscription[]> {
   return db('calendarEvents')
     .join('subscriptions', 'calendarEvents.subscriptionId', 'subscriptions.id')
     .select(
       'subscriptions.name as name',
       'subscriptions.category as category',
       'subscriptions.price as price',
+      'subscriptions.userAuthId as auth0Id',
       'calendarEvents.*'
     )
+    .where('auth0Id', id)
 }
 interface Prop {
   scheduleDate?: string
